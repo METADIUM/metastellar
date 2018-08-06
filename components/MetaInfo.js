5fac260c9482c72f7e770652d1abdf09011e73b8
hexoul <crosien@gmail.com>
2018-08-07 08:58:29 +0900

    Fix RSA keypair and update package.json

--- components/MetaInfo.js
+++ components/MetaInfo.js
@@ -1,9 +1,13 @@
 import React, {Component} from 'react';
 import {Button, Popup} from 'semantic-ui-react';
+import SimpleWebRTC from 'simplewebrtc';
 
 var QRCode = require('qrcode.react');
-var keypair = require('keypair');
+const NodeRSA = require('node-rsa');
+/*
 var crypto = require('crypto');
+crypto.privateDecrypt(PRIVKEY, Buffer.from(encmsg, 'base64'));
+*/
 
 class MetaInfo extends Component {
 
@@ -12,11 +16,13 @@ class MetaInfo extends Component {
   }
 
   componentDidMount() {
-    var pair = keypair()
-    var pubkey = pair.public
-      .replace('-----BEGIN RSA PUBLIC KEY-----', '')
-      .replace('-----END RSA PUBLIC KEY-----', '')
+    
+    const key = new NodeRSA({b: 2048});
+    var pubkey = key.exportKey('public')
+      .replace('-----BEGIN PUBLIC KEY-----', '')
+      .replace('-----END PUBLIC KEY-----', '')
       .replace(/\s+|\n\r|\n|\r$/gm, '')
+    
     this.baseRequestUri = "meta://information?request=name&request=email&service=https%3A%2F%2Fmetastellar.metadium.com&callback=http%3A%2F%2F13.125.251.87%2F3000/conn?p=";
     this.requestUri = this.baseRequestUri + 1 + "&public_key=" + encodeURIComponent(pubkey);
     console.log(this.requestUri)
@@ -40,6 +46,7 @@ class MetaInfo extends Component {
         position='bottom right'
         style={{padding: '2em'}}>
           <QRCode value={this.requestUri} size="128"/>
+          <div></div>
       </Popup>
     );
   }
