4c7b5fb027476ed3599ecdb8c2135fd040de78c9
hexoul <crosien@gmail.com>
2018-08-06 15:24:15 +0900

    Generate RSA key pair on js and encode base64

--- components/MetaInfo.js
+++ components/MetaInfo.js
@@ -2,13 +2,23 @@ import React, {Component} from 'react';
 import {Button, Popup} from 'semantic-ui-react';
 
 var QRCode = require('qrcode.react');
+var keypair = require('keypair');
+var crypto = require('crypto');
 
 class MetaInfo extends Component {
 
   constructor() {
-    super();
+    super()
+  }
+
+  componentDidMount() {
+    var pair = keypair()
+    var pubkey = pair.public
+      .replace('-----BEGIN RSA PUBLIC KEY-----', '')
+      .replace('-----END RSA PUBLIC KEY-----', '')
+      .replace(/\s+|\n\r|\n|\r$/gm, '')
     this.baseRequestUri = "meta://information?request=name&request=email&service=https%3A%2F%2Fmetastellar.metadium.com&callback=http%3A%2F%2F13.125.251.87%2F3000/conn?p=";
-    this.requestUri = this.baseRequestUri + 1;
+    this.requestUri = this.baseRequestUri + 1 + "&public_key=" + window.btoa(pubkey);
   }
 
   onOpenSetInfo() {
