0ece283286981e29761adc634ea1786548b87fa0
hexoul <crosien@gmail.com>
2018-08-16 17:44:26 +0900

    add testcode

--- components/Login.js
+++ components/Login.js
@@ -28,6 +28,11 @@ class Login extends Component {
     var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     for (var i = 0; i < 5; i++)
       text += possible.charAt(Math.floor(Math.random() * possible.length));
+    
+    /* test */
+    text = "ABCDE"
+    console.log('session', text)
+    
     return text;
   }
 
@@ -35,7 +40,46 @@ class Login extends Component {
     const key = new NodeRSA({b: 2048});
     this.pubkey = key.exportKey('public')
     this.privkey = key.exportKey('private')
-    var pubkey = key.exportKey('public')
+    
+    /* test */
+    this.pubkey = '-----BEGIN PUBLIC KEY-----' + '\n' +
+    'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAqfyf289+iHxMouSXCGo0' + '\n' +
+    'dUObSnGjFxFUfPwWBHIJzViMRXcbnFruA4gerp8Ha/GXTa3e5XUzWnR+E30WnpEO' + '\n' +
+    'GIuyyU5z1XqbBsn+LSOoM/TnLGKUKwsot8aUyYMxRjJUGhbwFFQJj60HP58TTrVO' + '\n' +
+    'S4ZzMfecwUDfOT1t+YR1jOfSv+Wbo6BhNdqiPb0kym/ayxis32DU1SJpIdFf7wwb' + '\n' +
+    'R18nYaFG5IGkkQZ4i/61TYOJgv673q9RgY5YTEOQJT8IylPxt5HDle+/sAUVR0Hj' + '\n' +
+    'QUCCK5rCDJLgbc5aquLC2yvE1C7bgm2/4kqZApAm4gdnbqso1P+W5DwRzWgeec+W' + '\n' +
+    'wwIDAQAB' + '\n' +
+    '-----END PUBLIC KEY-----';
+    this.privkey = '-----BEGIN RSA PRIVATE KEY-----' + '\n' +
+    'MIIEpAIBAAKCAQEAqfyf289+iHxMouSXCGo0dUObSnGjFxFUfPwWBHIJzViMRXcb' + '\n' +
+    'nFruA4gerp8Ha/GXTa3e5XUzWnR+E30WnpEOGIuyyU5z1XqbBsn+LSOoM/TnLGKU' + '\n' +
+    'Kwsot8aUyYMxRjJUGhbwFFQJj60HP58TTrVOS4ZzMfecwUDfOT1t+YR1jOfSv+Wb' + '\n' +
+    'o6BhNdqiPb0kym/ayxis32DU1SJpIdFf7wwbR18nYaFG5IGkkQZ4i/61TYOJgv67' + '\n' +
+    '3q9RgY5YTEOQJT8IylPxt5HDle+/sAUVR0HjQUCCK5rCDJLgbc5aquLC2yvE1C7b' + '\n' +
+    'gm2/4kqZApAm4gdnbqso1P+W5DwRzWgeec+WwwIDAQABAoIBAF8xfuBc4bJTUaOD' + '\n' +
+    'OeVXfIsS0jstSjTX2qWnkjjpF/4sEVmxav/zmAjYSL5nhoN3ptC0hvee26RTSmtl' + '\n' +
+    'iK5B80/ho7zCN3IcmAaERdhHeKGoC3G6vtkgBaxtA9OZqZVzB9AYelgX+8a/EikZ' + '\n' +
+    'z3krZW48G/Smgt1ik999PEIYYLAIE9RqIfcoCRXEpBfaf77bsWslDk9cGgorO08b' + '\n' +
+    'wYDvGhZdlI7jFrqQds2BoqmpWpm7JaFziiBWYVA3IR2Guh/p7bCuHUOdzlbntS+m' + '\n' +
+    'y46rt+oHEDVM0Os/N5yIWkBXIVsGOeuQxq9xkMLLX+lTniRRlmCrJNEhyh91bL5K' + '\n' +
+    'lAPmEIECgYEA6QpBsw7EczpFoUvqOX7QKpxXoYXxsrNhzocwiTtKwH2Z5Y7Zc5Fo' + '\n' +
+    'IUBI3lHC9t9gegQcV1wd0lOoFZHvbiRS14L4HCEIipf8/o0xEoYNzPF2t6d4RrZg' + '\n' +
+    'uOqZPo5HAJR8gM+AAoz8SOOpTeM54HmtHwsZJgH++YHPjaMsz+FYxwMCgYEAurwJ' + '\n' +
+    'cCpDRE2kJ6zJDUiSkNY6N0GBic+E0L5rA1pnlSIYDB4L30E4fNXnHHfMEVS2u1dz' + '\n' +
+    'rUUlvO8wRCHdKi5fi6rQUKXeAe5bsKTayEVJG0ltTBDMVBfifZbdM6fN+eHMCDwM' + '\n' +
+    'R5swMEwv9D2FuYHc+U7vDtc5pmF/1FV7sZl2BUECgYEAyRnzxjxJJRK1QdMMJl+b' + '\n' +
+    '7hmKubWn9Mk97wbUyKglAuWgp+vVFRj23jLmbwvpjjcHv2PKvUyd44ITu8F69/Za' + '\n' +
+    'kuXPjB6pi0hLp7NaFJ0gTapWA3h+n68E2q6AXe5TsVlx84qc8haOI6xqzu72ZBM8' + '\n' +
+    'ZVyjcBwq10/BN34HzsCtZtcCgYAXkQmj5UGOCjpwY2cQTRtqOg8o0BSYHt+FNLLu' + '\n' +
+    'vmcaNC5SwSwAZgvJ/XjoZ9oSSd2x8QyBTpu/p3Qw9REjdKzMRKd7tiM5nSpnNbcC' + '\n' +
+    '1XOvhOnNUsVmy4jjDrol/cSKB8wnMeTe2KARSFw499a7nb8Um7ih59m3vNBULOGq' + '\n' +
+    'fVKewQKBgQCME7JmWrzvp/vk40p1hwjyk0xtYXJZkt40b4icD3h0D4+XBSOJghfs' + '\n' +
+    'pgthoLxvLkNHSkvqBITlzK/7XZ7SaWcw8l7ba4yQrQ8+nJ60ZYeGJkcZwV5qcGaU' + '\n' +
+    'g7SQ/K86gtOnmpDAHDGAiGeUnz+61hUrB8WT+RJx8c90ePeNmJ/AkA==' + '\n' +
+    '-----END RSA PRIVATE KEY-----';
+    
+    var pubkey = this.pubkey
       .replace('-----BEGIN PUBLIC KEY-----', '')
       .replace('-----END PUBLIC KEY-----', '')
       .replace(/\s+|\n\r|\n|\r$/gm, '');
@@ -70,7 +114,7 @@ class Login extends Component {
   checkResponse() {
     https.request({
       host: '2g5198x91e.execute-api.ap-northeast-2.amazonaws.com',
-      path: '/test?key=' + this.state.session,
+      path: '/test?key=' + 'ABCDE', /* this.state.session, */
     }, (res) => {
       let data = '';
       res.on('data', (chunk) => {
