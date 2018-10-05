6d03e641fab0c2be226ecc272eee6f66a86f9165
hexoul <crosien@gmail.com>
2018-10-05 09:52:19 +0900

    add annotation for config.json ignored on git

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -1,4 +1,11 @@
 import Web3 from 'web3';
+/**
+ * credentials.json includes:
+ *   - url
+ *   - addr
+ *   - privkey
+ *   - contractAddr
+ */
 import credentials from '../static/data/credentials.json';
 
 const web3 = new Web3(new Web3.providers.HttpProvider(credentials.url));
